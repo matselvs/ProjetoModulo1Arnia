f2b898a6442f917ac9714c04471d254954d475fc
Matheus Silva <110196784+matselvs@users.noreply.github.com>
2023-05-02 22:42:18 -0300

    Update sessão.js

--- sessão.js
+++ sessão.js
@@ -1,5 +1,5 @@
 // faz uma solicitação HTTP GET para o servidor JSON para obter os detalhes do usuário atualmente conectado
-fetch('http://localhost:3000/users/1')
+fetch('https://renderapiprojetomodulo1.onrender.com/users/1')
   .then(response => response.json())
   .then(data => {
     // substitui o "@Daniel" pelo nome de usuário retornado pelo servidor JSON
